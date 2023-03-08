f18ef9eac97abfac1c6527a0f575879ee2e0d7d9
vanessa garcia <venessag0908@gmail.com>
2023-03-07 18:27:56 -0700

    array lab

--- index.js
+++ index.js
@@ -1,1 +1,47 @@
 // Write your solution here!
+
+const cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(name){
+    cats.push(name);
+    return cats;
+
+}
+
+function  destructivelyPrependCat(name){
+    cats.unshift(name);
+    return cats;
+}
+
+function destructivelyRemoveLastCat(){
+    cats.splice(-1);
+    return cats;
+}
+
+function destructivelyRemoveFirstCat(){
+    cats.shift();
+    return cats;
+}
+
+function appendCat(name){
+    const newCats = [...cats, name];
+    //newCats.push(name);
+    return newCats;
+}
+
+function prependCat(name){
+    const newCats = [name, ...cats];
+    return newCats;
+}
+
+function removeLastCat(){
+    const newCats = [...cats];
+    newCats.splice(-1);
+    return newCats;
+}
+
+function removeFirstCat(){
+    const newCats = [...cats];
+    newCats.shift();
+    return newCats;
+}
